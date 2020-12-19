07d4caff6d2dd8630476cd41a7aa9a1e0c72bd5a
Pablo Alexis Dominguez Grau <pabloadominguezgrau@gmail.com>
2020-12-18 23:05:13 -0500

    Set Home and About to work with templates

--- blog_django_project/blog/views.py
+++ blog_django_project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>About This Blog</h1>')
+    return render(request, 'blog/about.html')
