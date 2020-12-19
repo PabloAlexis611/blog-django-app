51b3a4a1e1120a3f069663d847b9db59672a07cf
Pablo Alexis Dominguez Grau <pabloadominguezgrau@gmail.com>
2020-12-18 22:58:16 -0500

    Set default page to Blog Home, add About

--- blog_django_project/blog/views.py
+++ blog_django_project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>About This Blog</h1>')
